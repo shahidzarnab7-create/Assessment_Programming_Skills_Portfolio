c5f8293e33e00ea1ff33b7ce3de8528a751e1af8
shahidzarnab7-create <shahidzarnab7@gmail.com>
2026-01-07 13:58:38 +0300

    simple search functionality for names

---
+++ Exercise_08 /simple_search.py
@@ -0,0 +1,9 @@
+#Excercise 8 Simple Serch 
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+search = input("Enter name: ")
+
+if search in names:
+    print("Found")
+else:
+    print("Not Found")
