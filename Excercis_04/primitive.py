4f67d0bb8d6c292d8892927c8d48719c22b37ce6
shahidzarnab7-create <shahidzarnab7@gmail.com>
2026-01-07 11:09:51 +0300

    Delete Excercis_04 directory

--- Excercis_04/primitive.py
+++
@@ -1,9 +0,0 @@
-#Exercise 4: Primitive Quiz 
-# This program asks a quiz question and checks the answer
-
-answer = input("What is the capital of France? ")
-
-if answer.strip().lower() == "paris":
-    print("Correct!")
-else:
-    print("Wrong! The correct answer is Paris.")
