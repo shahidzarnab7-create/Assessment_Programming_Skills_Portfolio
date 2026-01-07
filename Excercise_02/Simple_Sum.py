0ee6109a6d0fb326be9da1b2f8057c7b5157e970
shahidzarnab7-create <shahidzarnab7@gmail.com>
2026-01-07 10:42:33 +0300

    Simple_Sum.py

---
+++ Excercise_02/Simple_Sum.py
@@ -0,0 +1,8 @@
+# Exercise 2 simple sum
+# x & y are the variable holding the values
+number_1 = 8
+number_2 = 10
+# result varialbe is holding the calculated value
+result = number_1 + number_2
+# Displaying the result variable value
+print(result)
