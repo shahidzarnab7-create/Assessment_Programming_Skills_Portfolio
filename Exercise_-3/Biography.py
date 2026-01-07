7a704ba4553a1947e1920e8528dffdd322de288a
shahidzarnab7-create <shahidzarnab7@gmail.com>
2026-01-07 11:03:33 +0300

    Biography.py

---
+++ Exercise_-3/Biography.py
@@ -0,0 +1,10 @@
+# Exercise 3: Biography 
+# This program stores information (name, hometown, age)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+hometown = input("Enter your hometown: ")
+
+print("\nName:", name)
+print("Age:", age)
+print("Hometown:", hometown)
